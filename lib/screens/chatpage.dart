dc8890c799a2af50d174b189d26f3586c3995c78
jarraju03 <jarraju03@gmail.com>
2024-02-19 13:00:48 +0000

    commit to check pckl device

--- lib/screens/chatpage.dart
+++ lib/screens/chatpage.dart
@@ -140,6 +140,30 @@ class _ChatPage extends State<ChatPage> {
           Container(
             color: Color.fromARGB(255, 213, 213, 214),
             height: 75,
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.only(left: 15, right: 5),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Enter your message",
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.send_rounded,
+                          color: Color(0xFF6E00FF),
+                          size: 30,
+                        ))
+                  ],
+                ),
+              ),
+            ),
           )
         ],
       ),
